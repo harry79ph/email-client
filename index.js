a21444552ac143292313dfcf5a59a1cbde9a24f2
harry79ph <serdare_79@hotmail.com>
2022-10-11 22:30:16 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+require("dotenv").config();
+const nodemailer = require("nodemailer");
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ msg: "working" });
+});
+
+app.post("/email", (req, res) => {
+  const { name, email, subject, message } = req.body;
+  let transporter = nodemailer.createTransport({
+    service: process.env.MAIL_SERVICE,
+    auth: {
+      user: process.env.USER_EMAIL,
+      pass: process.env.USER_PASS,
+    },
+  });
+  const htmlHandler = (name, email, message) =>
+    `<h3 style="color:#a93226;">${name}, from ${email}</h3><p style="color:#1b2631;">${message}</p>`;
+  const mailOptions = {
+    from: email,
+    to: process.env.RECEIVER_EMAIL,
+    subject: subject,
+    html: htmlHandler(name, email, message),
+  };
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.json({ msg: "error" });
+    } else {
+      console.log("Email sent: " + info.response);
+      res.json({ msg: "success" });
+    }
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Running on port ${PORT}`);
+});
